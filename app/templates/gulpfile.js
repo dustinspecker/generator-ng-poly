f772351a51c1ea615031bf2a5e56b04af098a94f
Dustin Specker <DustinSpecker@DustinSpecker.com>
2014-07-14 22:28:00 -0500

    clean up Gulpfile and use Karma failed reporter

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -14,6 +14,46 @@ var _ = require('lodash')
   , path = require('path')
   , rimraf = require('rimraf');
 
+// bower assets to be inject into index.html
+// 'bower_components' is automatically prepended
+var injectableBowerComponents = [
+  'angular/angular.js',
+  'angular-ui-router/release/angular-ui-router.js',
+  'platform/platform.js'
+];
+
+// bower polymer components that do not need to injected into index.html
+// 'bower_components' is automatically prepended
+var bowerPolymerComponents = [
+  'polymer/polymer.{js,html}',
+  'polymer/layout.html'
+];
+
+// src files
+var componentsBase = 'src/components/'
+  , componentsDir = componentsBase + '**/'
+  , componentsJade = componentsDir + '*.jade'
+  , componentsJs = componentsDir + '*.js'
+  , componentsLess = componentsDir + '*.less'
+  , srcJadeFiles = 'src/jade/**/*.jade'
+  , srcJadeTemplates = 'src/jade/templates/*.jade' // used for karmaConf
+  , srcJsFiles = 'src/js/**/*.js'
+  , srcLessDir = 'src/less/'; // since we need to strictly specify style.less later
+
+// test files
+var unitTests = 'tests/unit/**/*.spec.js';
+
+// build files
+var build = 'build/'
+  , buildComponents = build + 'components/'
+  , buildCss = build + 'css/'
+  , buildJs = build + 'js/';
+
+var bowerDir = 'bower_components/';
+function prependBowerDir(file) {
+  return bowerDir + file;
+}
+
 var karmaConf = {
   browsers: ['PhantomJS'],
   frameworks: ['jasmine'],
@@ -21,11 +61,11 @@ var karmaConf = {
     'bower_components/angular/angular.js',
     'bower_components/angular-ui-router/release/angular-ui-router.js',
     'bower_components/angular-mocks/angular-mocks.js',
-    'src/js/**/*.js',
-    'tests/unit/**/*.spec.js',
-    'src/jade/templates/*.jade'
+    srcJsFiles,
+    unitTests,
+    srcJadeTemplates
   ],
-  reporters: ['progress', 'coverage'],
+  reporters: ['failed', 'coverage'],
   preprocessors: {
     'src/js/**/*.js': ['coverage'],
     'src/jade/templates/*.jade': ['ng-jade2js']
@@ -41,69 +81,66 @@ gulp.task('clean', function (cb) {
 });
 
 gulp.task('components', ['clean', 'jshint'], function () {
-  gulp.src('src/components/**/*.js', {
-      base: 'src/components'
+  gulp.src(componentsJs, {
+      base: componentsBase
     })
-    .pipe(gulp.dest('build/components/'));
+    .pipe(gulp.dest(buildComponents));
 
-  gulp.src('src/components/**/*.less', {
-      base: 'src/components'
+  gulp.src(componentsLess, {
+      base: componentsBase
     })
     .pipe(less())
-    .pipe(gulp.dest('build/components/'));
+    .pipe(gulp.dest(buildComponents));
 
-  return gulp.src('src/components/**/*.jade', {
-      base: 'src/components'
+  return gulp.src(componentsJade, {
+      base: componentsBase
     })
     .pipe(jade())
-    .pipe(gulp.dest('build/components'));
+    .pipe(gulp.dest(buildComponents));
 });
 
 gulp.task('connect', function () {
   connect.server({
-    root: 'build',
+    root: build,
     port: 8080,
     livereload: true
   });
 });
 
 gulp.task('jade', ['clean'], function () {
-  return gulp.src('src/jade/**/*.jade')
+  return gulp.src(srcJadeFiles)
     .pipe(jade())
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest(build));
 });
 
 gulp.task('inject', ['jade', 'js', 'less', 'components'], function () {
-  return gulp.src('build/index.html')
+  return gulp.src(build + 'index.html')
     .pipe(inject(gulp.src([
-      'build/components/**/*.html',
-      'build/css/*.css',
-      'build/js/angular.js',
-      'build/js/angular-ui-router.js',    
-      'build/js/**/*.js'
+      buildComponents + '**/*.html',
+      buildCss + '*.css',
+      buildJs + 'angular.js',
+      buildJs + 'angular-ui-router.js',    
+      buildJs + '**/*.js'
       ], { read: false }), {
         addRootSlash: false,
-        ignorePath: 'build/'
+        ignorePath: build
     }))
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest(build))
     .pipe(connect.reload());
 });
 
 gulp.task('js', ['clean', 'jshint'], function () {
   return gulp.src([
-      'src/js/**/*.js',
-      'bower_components/angular/angular.js',
-      'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/platform/platform.js'
-    ])
+      srcJsFiles
+    ].concat(injectableBowerComponents.map(prependBowerDir)))
     .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('jshint', function () {
   return gulp.src([
-    'src/components/**/*.js',
-    'src/js/**/*.js',
-    'tests/unit/**/*.spec.js'
+    componentsJs,
+    srcJsFiles,
+    unitTests
     ])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
@@ -112,11 +149,11 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('less', ['clean'], function () {
-  return gulp.src('src/less/style.less')
+  return gulp.src(srcLessDir + 'style.less')
     .pipe(less({
-      paths: [path.join(__dirname, 'src/less', 'src/less/includes')]
+      paths: [path.join(__dirname, srcLessDir, srcLessDir + 'includes')]
     }))
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest(buildCss));
 });
 
 gulp.task('open', function () {
@@ -126,13 +163,10 @@ gulp.task('open', function () {
 });
 
 gulp.task('polymer', ['inject'], function () {
-  return gulp.src([
-    'bower_components/polymer/polymer.{js,html}',
-    'bower_components/polymer/layout.html'
-    ], {
-      base: 'bower_components'
+  return gulp.src(bowerPolymerComponents.map(prependBowerDir), {
+      base: bowerDir
     })
-    .pipe(gulp.dest('build/components'));
+    .pipe(gulp.dest(buildComponents));
 });
 
 gulp.task('build', ['polymer']);
@@ -148,6 +182,6 @@ gulp.task('test', ['jshint'], function (done) {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['tests/unit/**/*.spec.js'], ['test']);
-  gulp.watch(['src/jade/**/*.jade', 'src/js/**/*.js'], ['build']);
+  gulp.watch([unitTests], ['test']);
+  gulp.watch([srcJadeFiles, srcJsFiles, srcLessDir + '**/*.less', componentsDir + '*.{jade,js,less}'], ['build']);
 });
