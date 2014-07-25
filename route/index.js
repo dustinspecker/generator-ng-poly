9c0a9e3ce55b6088601b0d83b6b7c57d40c29586
Dustin Specker <DustinSpecker@DustinSpecker.com>
2014-07-25 17:37:09 -0500

    generalize route addition

--- route/index.js
+++ route/index.js
@@ -20,11 +20,32 @@ Generator.prototype.writing = function writing() {
 
   // find line to add new state
   var lines = file.split(endOfLine)
-    , lineIndex = 0;
+    , stateStartIndex = -1
+    , stateEndIndex = -1
+    , braceCount = 0; // {}
   lines.forEach(function (line, i) {
-    if (line.indexOf('controller:') !== -1) {
-      lineIndex = i;
+    // look for .state and set stateStartIndex
+    if (line.indexOf('.state(') > -1) {
+      stateStartIndex = i;
     }
+
+    // open braces add to braceCount
+    if (stateStartIndex > -1 && line.indexOf('{') > -1) {
+      braceCount++;
+    }
+
+    // close braces subract from braceCount
+    if (stateStartIndex > -1 && line.indexOf('}') > -1) {
+      braceCount--;
+    }
+
+    // when braceCount = 0 the end of the state has been reached
+    // set stateEndIndex
+    if (stateStartIndex > -1 && braceCount === 0) {
+      stateEndIndex = i;
+    }
+
+    // loop through everything to append new route at the end
   });
 
   // create new state
@@ -36,8 +57,9 @@ Generator.prototype.writing = function writing() {
     '      controller: \'' + config.ctrlName + '\'',
   ];
 
-  // insert new state and join all lines
-  lines.splice(lineIndex+1, 0, newState.map(function (line) {
+  // join the state
+  // insert state above }); of the original last state
+  lines.splice(stateEndIndex, 0, newState.map(function (line) {
     return line;
   }).join(endOfLine));
 
