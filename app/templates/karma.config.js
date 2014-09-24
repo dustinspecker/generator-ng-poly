a57a72e13c09c0bf5c66ec7ff7a3573456a944f3
Dustin Specker <DustinSpecker@DustinSpecker.com>
2014-09-24 17:40:59 -0500

    (fix) karma preprocessors use appDir
    Karma preprocessors now use appDir from build.config.js.

--- app/templates/karma.config.js
+++ app/templates/karma.config.js
@@ -1,24 +1,35 @@
+'use strict';
+var buildConfig = require('./build.config.js')
+  , preprocessors = {}
+  , appDir;
+
+  appDir = buildConfig.appDir;
+  // add slash if missing to properly strip prefix from directive templates
+  if (appDir[appDir.length - 1] !== '/') {
+    appDir = appDir + '/';
+  }
+
+  preprocessors[appDir + '**/!(*_test)+(.js)'] = ['coverage'];
+  preprocessors[appDir + '**/*-directive.tpl.haml'] =  ['ng-haml2js'];
+  preprocessors[appDir + '**/*-directive.tpl.html'] = ['ng-html2js'];
+  preprocessors[appDir + '**/*-directive.tpl.jade'] = ['ng-jade2js'];
+  preprocessors['**/*.coffee'] = ['coffee'];
+
 module.exports = {
   browsers: ['PhantomJS'],
   frameworks: ['jasmine'],
   files: [
   ],
   reporters: ['failed', 'coverage'],
-  preprocessors: {
-    'app/**/!(*_test)+(.js)': ['coverage'],
-    'app/**/*-directive.tpl.haml': ['ng-haml2js'],
-    'app/**/*-directive.tpl.html': ['ng-html2js'],
-    'app/**/*-directive.tpl.jade': ['ng-jade2js'],
-    '**/*.coffee': ['coffee']
-  },
+  preprocessors: preprocessors,
   ngHaml2JsPreprocessor: {
-    stripPrefix: 'app/'
+    stripPrefix: appDir
   },
   ngHtml2JsPreprocessor: {
-    stripPrefix: 'app/'
+    stripPrefix: appDir
   },
   ngJade2JsPreprocessor: {
-    stripPrefix: 'app/'
+    stripPrefix: appDir
   },
   singleRun: true
 };
