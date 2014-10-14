68a21e7f0a73abe69d94d147d963f96f7e8c5a18
Dustin Specker <DustinSpecker@DustinSpecker.com>
2014-10-14 17:50:29 -0500

    (feat) add support for Angular 1.3
    ng-poly:app asks to use 1.2.* or 1.3.*.

--- app/index.js
+++ app/index.js
@@ -13,219 +13,237 @@ Generator.prototype.prompting = function prompting() {
 
   // ask for app name
   // get preferred langugaes
-  this.prompt([{
-    name: 'appName',
-    message: 'What is the app\'s name?',
-    validate: function (input) {
-      // require an app name
-      return !!input;
+  this.prompt([
+    {
+      name: 'appName',
+      message: 'What is the app\'s name?',
+      validate: function (input) {
+        // require an app name
+        return !!input;
+      }
+    },
+    {
+      type: 'list',
+      name: 'ngversion',
+      message: 'Which version of Angular should be used?',
+      choices: [
+        {
+          name: '1.2.*',
+          value: '1.2.*'
+        },
+        {
+          name: '1.3.*',
+          value: '1.3.*'
+        }
+      ]
+    },
+    {
+      name: 'host',
+      message: 'What host should the app run on?',
+      default: 'localhost'
+    },
+    {
+      name: 'port',
+      message: 'Which port should the app run on?',
+      default: 3000
+    },
+    {
+      name: 'appDir',
+      message: 'Which folder should the app be developed in?',
+      default: 'app'
+    },
+    {
+      type: 'list',
+      name: 'markup',
+      message: 'Which is the preferred markup language?',
+      default: 'jade',
+      choices: [
+        {
+          name: 'HAML',
+          value: 'haml'
+        },
+        {
+          name: 'HTML',
+          value: 'html'
+        },
+        {
+          name: 'Jade',
+          value: 'jade'
+        }
+      ]
+    },
+    {
+      type: 'list',
+      name: 'appScript',
+      message: 'Which is the preferred application scripting language?',
+      default: 'js',
+      choices: [
+        {
+          name: 'CoffeeScript',
+          value: 'coffee'
+        },
+        {
+          name: 'JavaScript',
+          value: 'js'
+        }
+      ]
+    },
+    {
+      type: 'confirm',
+      name: 'controllerAs',
+      message: 'Want to use Controller As syntax?',
+      default: true
+    },
+    {
+      type: 'confirm',
+      name: 'passFunc',
+      message: 'Should functions be defined and passed instead of defined inline (in callbacks)?',
+      default: true
+    },
+    {
+      type: 'confirm',
+      name: 'namedFunc',
+      message: 'Want to use named functions instead of anonymous?',
+      default: true
+    },
+    {
+      name: 'unitTestDir',
+      message: 'Where should unit tests be saved?',
+      default: 'app'
+    },
+    {
+      type: 'list',
+      name: 'testScript',
+      message: 'Which is the preferred test scripting language?',
+      default: 'js',
+      choices: [
+        {
+          name: 'CoffeeScript',
+          value: 'coffee'
+        },
+        {
+          name: 'JavaScript',
+          value: 'js'
+        }
+      ]
+    },
+    {
+      type: 'list',
+      name: 'testFramework',
+      message: 'Which is the preferred unit testing framework?',
+      default: 'jasmine',
+      choices: [
+        {
+          name: 'Jasmine',
+          value: 'jasmine'
+        },
+        {
+          name: 'Mocha with Chai',
+          value: 'mocha'
+        }
+      ]
+    },
+    {
+      type: 'list',
+      name: 'style',
+      message: 'Which is the preferred style language?',
+      default: 'less',
+      choices: [
+        {
+          name: 'CSS',
+          value: 'css'
+        },
+        {
+          name: 'LESS',
+          value: 'less'
+        },
+        {
+          name: 'SCSS',
+          value: 'scss'
+        },
+        {
+          name: 'Stylus',
+          value: 'styl'
+        }
+      ]
+    },
+    {
+      type: 'confirm',
+      name: 'polymer',
+      message: 'Should Polymer support be enabled?',
+      default: true
+    },
+    {
+      type: 'list',
+      name: 'framework',
+      message: 'Should a framework be setup?',
+      choices: [
+        {
+          name: 'none',
+          value: 'none'
+        },
+        {
+          name: 'Bootstrap with AngularStrap',
+          value: 'angularstrap'
+        },
+        {
+          name: 'Bootstrap with UI Bootstrap',
+          value: 'uibootstrap'
+        },
+        {
+          name: 'Foundation with Angular Foundation',
+          value: 'foundation'
+        }
+      ]
+    },
+    {
+      type: 'confirm',
+      name: 'ngRoute',
+      message: 'Should ngRoute be used instead of UI Router?',
+      default: false
+    },
+    {
+      type: 'checkbox',
+      name: 'bower',
+      message: 'Which additonal Bower components should be installed?',
+      choices: [
+        {
+          name: 'Angular Animate',
+          value: 'animate'
+        },
+        {
+          name: 'Angular Cookies',
+          value: 'cookies'
+        },
+        {
+          name: 'Angular Resource',
+          value: 'resource'
+        },
+        {
+          name: 'Angular Sanitize',
+          value: 'sanitize'
+        },
+        {
+          name: 'Angular Touch',
+          value: 'touch'
+        },
+        {
+          name: 'Font Awesome',
+          value: 'fontawesome'
+        },
+        {
+          name: 'Lo-Dash',
+          value: 'lodash'
+        },
+        {
+          name: 'Restangular (installs Lo-Dash)',
+          value: 'restangular'
+        }
+      ]
     }
-  },
-  {
-    name: 'host',
-    message: 'What host should the app run on?',
-    default: 'localhost'
-  },
-  {
-    name: 'port',
-    message: 'Which port should the app run on?',
-    default: 3000
-  },
-  {
-    name: 'appDir',
-    message: 'Which folder should the app be developed in?',
-    default: 'app'
-  },
-  {
-    type: 'list',
-    name: 'markup',
-    message: 'Which is the preferred markup language?',
-    default: 'jade',
-    choices: [
-      {
-        name: 'HAML',
-        value: 'haml'
-      },
-      {
-        name: 'HTML',
-        value: 'html'
-      },
-      {
-        name: 'Jade',
-        value: 'jade'
-      }
-    ]
-  },
-  {
-    type: 'list',
-    name: 'appScript',
-    message: 'Which is the preferred application scripting language?',
-    default: 'js',
-    choices: [
-      {
-        name: 'CoffeeScript',
-        value: 'coffee'
-      },
-      {
-        name: 'JavaScript',
-        value: 'js'
-      }
-    ]
-  },
-  {
-    type: 'confirm',
-    name: 'controllerAs',
-    message: 'Want to use Controller As syntax?',
-    default: true
-  },
-  {
-    type: 'confirm',
-    name: 'passFunc',
-    message: 'Should functions be defined and passed instead of defined inline (in callbacks)?',
-    default: true
-  },
-  {
-    type: 'confirm',
-    name: 'namedFunc',
-    message: 'Want to use named functions instead of anonymous?',
-    default: true
-  },
-  {
-    name: 'unitTestDir',
-    message: 'Where should unit tests be saved?',
-    default: 'app'
-  },
-  {
-    type: 'list',
-    name: 'testScript',
-    message: 'Which is the preferred test scripting language?',
-    default: 'js',
-    choices: [
-      {
-        name: 'CoffeeScript',
-        value: 'coffee'
-      },
-      {
-        name: 'JavaScript',
-        value: 'js'
-      }
-    ]
-  },
-  {
-    type: 'list',
-    name: 'testFramework',
-    message: 'Which is the preferred unit testing framework?',
-    default: 'jasmine',
-    choices: [
-      {
-        name: 'Jasmine',
-        value: 'jasmine'
-      },
-      {
-        name: 'Mocha with Chai',
-        value: 'mocha'
-      }
-    ]
-  },
-  {
-    type: 'list',
-    name: 'style',
-    message: 'Which is the preferred style language?',
-    default: 'less',
-    choices: [
-      {
-        name: 'CSS',
-        value: 'css'
-      },
-      {
-        name: 'LESS',
-        value: 'less'
-      },
-      {
-        name: 'SCSS',
-        value: 'scss'
-      },
-      {
-        name: 'Stylus',
-        value: 'styl'
-      }
-    ]
-  },
-  {
-    type: 'confirm',
-    name: 'polymer',
-    message: 'Should Polymer support be enabled?',
-    default: true
-  },
-  {
-    type: 'list',
-    name: 'framework',
-    message: 'Should a framework be setup?',
-    choices: [
-      {
-        name: 'none',
-        value: 'none'
-      },
-      {
-        name: 'Bootstrap with AngularStrap',
-        value: 'angularstrap'
-      },
-      {
-        name: 'Bootstrap with UI Bootstrap',
-        value: 'uibootstrap'
-      },
-      {
-        name: 'Foundation with Angular Foundation',
-        value: 'foundation'
-      }
-    ]
-  },
-  {
-    type: 'confirm',
-    name: 'ngRoute',
-    message: 'Should ngRoute be used instead of UI Router?',
-    default: false
-  },
-  {
-    type: 'checkbox',
-    name: 'bower',
-    message: 'Which additonal Bower components should be installed?',
-    choices: [
-      {
-        name: 'Angular Animate',
-        value: 'animate'
-      },
-      {
-        name: 'Angular Cookies',
-        value: 'cookies'
-      },
-      {
-        name: 'Angular Resource',
-        value: 'resource'
-      },
-      {
-        name: 'Angular Sanitize',
-        value: 'sanitize'
-      },
-      {
-        name: 'Angular Touch',
-        value: 'touch'
-      },
-      {
-        name: 'Font Awesome',
-        value: 'fontawesome'
-      },
-      {
-        name: 'Lo-Dash',
-        value: 'lodash'
-      },
-      {
-        name: 'Restangular (installs Lo-Dash)',
-        value: 'restangular'
-      }
-    ]
-  }], function (props) {
+  ], function (props) {
     this.appName = props.appName;
+    this.ngversion = props.ngversion;
     this.appDir = props.appDir;
     this.host = props.host;
     this.port = props.port;
@@ -272,6 +290,7 @@ Generator.prototype.configuring = function configuring() {
 
   this.context = {
     appName: this.appName,
+    ngversion: this.ngversion,
     appDir: this.appDir,
     unitTestDir: this.unitTestDir,
     host: this.host,
