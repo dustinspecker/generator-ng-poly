3ea04c4661ab6f79e2cd3b536c30ab60143c95a1
Dustin Specker <dustinspecker@dustinspecker.com>
2014-12-24 17:56:50 -0600

    (feat) add ngAria support

--- app/templates/_app.ts
+++ app/templates/_app.ts
@@ -10,7 +10,8 @@ module <%= moduleName %> {
    *
    */
   angular
-    .module('<%= moduleName %>', [
+    .module('<%= moduleName %>', [<% if (bower.indexOf('aria') > -1) {  %>
+      'ngAria',<% } %>
       <% if (ngRoute) { %>'ngRoute'<% } else { %>'ui.router'<% } %><% if (framework === 'angularstrap') { %>,
       'mgcrea.ngStrap'<% } %><% if (framework === 'uibootstrap') { %>,
       'ui.bootstrap'<% } %><% if (framework === 'foundation') { %>,
