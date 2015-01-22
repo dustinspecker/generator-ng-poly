059fecf5cf8519e60659c7fed42158805dffbb55
Dustin Specker <DustinSpecker@DustinSpecker.com>
2015-01-21 18:44:05 -0600

    (fix) use chai-as-promised

--- route/templates/page_test.js
+++ route/templates/page_test.js
@@ -4,8 +4,11 @@
 var buildConfigFile = require('findup-sync')('build.config.js')
   , buildConfig = require(buildConfigFile)<% if (e2eTestFramework === 'mocha') { %>
   , chai = require('chai')
+  , chaiAsPromised = require('chai-as-promised')
   , expect = chai.expect<% } %>
-  , <%= upperCamel %>PagePo = require('./<%= hyphenName %>.po');
+  , <%= upperCamel %>PagePo = require('./<%= hyphenName %>.po');<% if (e2eTestFramework === 'mocha') { %>
+
+chai.use(chaiAsPromised);<% } %>
 
 describe('<%= humanName %> page', function () {
   var <%= lowerCamel %>Page;
