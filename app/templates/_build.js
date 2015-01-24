499c49af6e698fa1ca84f8b2bd10ec4bb83f5052
Dustin Specker <DustinSpecker@DustinSpecker.com>
2015-01-24 11:12:55 -0600

    fix(polymer) remove template from gulp/build
    Looks like I accidentally copied over a template into _build.js.

--- app/templates/_build.js
+++ app/templates/_build.js
@@ -252,39 +252,7 @@ gulp.task('components', ['bowerInject'], function () {
   return gulp.src(appComponents)<% if (polymer) { %>
     .pipe($.addSrc(bowerDir + 'polymer/{layout,polymer}.{html,js}', {base: bowerDir}))<% } %>
     .pipe(typeScriptFilter)
-    .pipe($.typescript(<% if (passFunc) { %>(function () {
-      <% } %>'use strict';
-
-    <% if (passFunc) { %>  <% } %>/**
-    <% if (passFunc) { %>  <% } %> * @ngdoc service
-    <% if (passFunc) { %>  <% } %> * @name <% if (parentModuleName) { %><%= parentModuleName %>.<% } %><%= moduleName %>.factory:<%= upperCamel %>
-    <% if (passFunc) { %>  <% } %> *
-    <% if (passFunc) { %>  <% } %> * @description
-    <% if (passFunc) { %>  <% } %> *
-    <% if (passFunc) { %>  <% } %> */
-    <% if (passFunc) { %>  <% } %>angular
-    <% if (passFunc) { %>  <% } %>  .module('<% if (parentModuleName) { %><%= parentModuleName %>.<% } %><%= moduleName %>')<% if (passFunc) { %>
-    <% if (passFunc) { %>  <% } %>  .factory('<%= upperCamel %>', <%= upperCamel %>);<% } else { %>
-    <% if (passFunc) { %>  <% } %>  .factory('<%= upperCamel %>', function <% if (namedFunc) { %><%= upperCamel %><% } %>() {
-    <% if (passFunc) { %>  <% } %>    var <%= upperCamel %>Base = {};
-    <% if (passFunc) { %>  <% } %>    <%= upperCamel %>Base.someValue = '<%= upperCamel %>';
-    <% if (passFunc) { %>  <% } %>    <%= upperCamel %>Base.someMethod = function <% if (namedFunc) { %>someMethod<% } %>() {
-    <% if (passFunc) { %>  <% } %>      return '<%= upperCamel %>';
-    <% if (passFunc) { %>  <% } %>    };
-    <% if (passFunc) { %>  <% } %>    return <%= upperCamel %>Base;
-    <% if (passFunc) { %>  <% } %>  });<% } %><% if (passFunc) { %>
-
-      function <%= upperCamel %>() {
-        var <%= upperCamel %>Base = {};
-        <%= upperCamel %>Base.someValue = '<%= upperCamel %>';
-        <%= upperCamel %>Base.someMethod = function <% if (namedFunc) { %>someMethod<% } %>() {
-          return '<%= upperCamel %>';
-        };
-        return <%= upperCamel %>Base;
-      }
-
-    })();<% } %>
-))
+    .pipe($.typescript())
     .pipe(typeScriptFilter.restore())
     .pipe(coffeeFilter)
     .pipe($.coffee())
