6b0aed2da5f06b429005d329cbaf2301f3c7281b
Dustin Specker <DustinSpecker@DustinSpecker.com>
2015-01-24 07:29:44 -0600

    feat(gen) add gulp tasks for linting and testing

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+var gulp = require('gulp')
+  , istanbul = require('gulp-istanbul')
+  , jscs = require('gulp-jscs')
+  , jshint = require('gulp-jshint')
+  , mocha = require ('gulp-mocha')
+
+  , srcFiles = '*/*.js'
+  , testFiles = 'test/*.js';
+
+gulp.task('lint', function () {
+  return gulp.src([srcFiles, testFiles])
+    .pipe(jscs())
+    .pipe(jshint());
+});
+
+gulp.task('test', ['lint'], function (cb) {
+  gulp.src([srcFiles, '!' + testFiles])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src([testFiles])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
+});
