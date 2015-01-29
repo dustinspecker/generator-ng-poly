acb4b2d0095bbf27b89e973d167c04bfd16f0e1d
Dustin Specker <DustinSpecker@DustinSpecker.com>
2015-01-29 17:58:03 -0600

    fix(test) correct template path

--- app/templates/_karma.config.js
+++ app/templates/_karma.config.js
@@ -1,24 +1,15 @@
 'use strict';
 var buildConfig = require('./build.config.js')
-  , isProd = require('yargs').argv.stage === 'prod'
   , preprocessors = {}
-  , buildDir
   , buildTestDir
   , templateDir
   , jsDir;
-
-buildDir = buildConfig.buildDir;
-// add slash if missing to properly strip prefix from directive templates
-if (buildDir[buildDir.length - 1] !== '/') {
-  buildDir = buildDir + '/';
-}
 
 buildTestDir = buildConfig.buildTestDir;
 // add slash if missing to properly strip prefix from directive templates
 if (buildTestDir[buildTestDir.length - 1] !== '/') {
   buildTestDir = buildTestDir + '/';
 }
-
 templateDir = buildTestDir + 'templates/';
 
 jsDir = buildConfig.buildJs;
@@ -27,7 +18,7 @@ if (jsDir[jsDir.length - 1] !== '/') {
   jsDir = jsDir + '/';
 }
 
-preprocessors[jsDir + '**/*.js)'] = ['coverage'];
+preprocessors[jsDir + '**/*.js'] = ['coverage'];
 preprocessors[templateDir + '**/*-directive.tpl.html'] = ['ng-html2js'];
 
 module.exports = {
@@ -36,7 +27,7 @@ module.exports = {
   reporters: ['failed', 'coverage'],
   preprocessors: preprocessors,
   ngHtml2JsPreprocessor: {
-    stripPrefix: buildDir
+    stripPrefix: templateDir
   },
   singleRun: true
 };
