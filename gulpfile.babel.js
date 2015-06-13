50114cc86d6a6026ab7c778575711b2e892c34ed
Dustin Specker <dustinspecker@dustinspecker.com>
2015-06-12 22:49:26 -0500

    chore(gulp) convert Gulpfile to ES2015

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,30 +1,33 @@
 'use strict';
-var gulp = require('gulp')
-  , eslint = require('gulp-eslint')
-  , istanbul = require('gulp-istanbul')
-  , jscs = require('gulp-jscs')
-  , jshint = require('gulp-jshint')
-  , mocha = require ('gulp-mocha')
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import istanbul from 'gulp-istanbul';
+import jscs from 'gulp-jscs';
+import jshint from 'gulp-jshint';
+import mocha from 'gulp-mocha';
 
+const configFiles = 'gulpfile.babel.js'
   , srcFiles = '*/*.js'
   , testFiles = 'test/*.js';
 
-gulp.task('lint', function () {
-  return gulp.src([srcFiles, testFiles])
+gulp.task('lint', () => {
+  return gulp.src([configFiles, srcFiles, testFiles])
     .pipe(eslint())
     .pipe(eslint.formatEach('./node_modules/eslint-path-formatter'))
     .pipe(eslint.failOnError())
-    .pipe(jscs())
+    .pipe(jscs({
+      esnext: true
+    }))
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('test', ['lint'], function (cb) {
+gulp.task('test', ['lint'], (cb) => {
   gulp.src([srcFiles, '!' + testFiles])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
-    .on('finish', function () {
+    .on('finish', () => {
       gulp.src([testFiles])
         .pipe(mocha())
         .pipe(istanbul.writeReports())
