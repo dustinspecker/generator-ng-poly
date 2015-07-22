872ef5f69939565288d02378f9394db7bed010bc
Dustin Specker <dustinspecker@dustinspecker.com>
2015-07-21 19:52:09 -0500

    refactor(utils/route) use func-has-param
    Don't use func-has-param for CoffeeScript. func-has-param is
    fragile and cases such as `.config ->` callbacks will cause an error.
    If that case was `.config () ->` then func-has-param would work.
    
    Look into using func-has-param's regex option to create a regex
    with optional ()'s.

--- test/test-route.js
+++ test/test-route.js
@@ -4,40 +4,38 @@ import {assert, test as helpers} from 'yeoman-generator';
 import {join} from 'path';
 
 describe('Route generator', () => {
-  before((done) => {
-    helpers
-      .run(join(__dirname, '../generators/app'))
-      .withPrompts({
-        appName: 'temp-route',
-        markup: 'html',
-        appScript: 'js',
-        controllerAs: false,
-        testScript: 'js',
-        testDir: 'app',
-        style: 'less',
-        bower: []
-      })
-      .withGenerators([
-        join(__dirname, '../generators/module'),
-        join(__dirname, '../generators/route'),
-        join(__dirname, '../generators/controller'),
-        join(__dirname, '../generators/view')
-      ])
-      .on('end', done);
-  });
-
   describe('with HTML markup, Less style, JS app, JS test, and skipController', () => {
     before((done) => {
       helpers
-        .run(join(__dirname, '../generators/route'), {
-          tmpdir: false
+        .run(join(__dirname, '../generators/app'))
+        .withPrompts({
+          appName: 'temp-route',
+          markup: 'html',
+          appScript: 'js',
+          controllerAs: false,
+          testScript: 'js',
+          testDir: 'app',
+          style: 'less',
+          bower: []
         })
-        .withArguments(['test'])
-        .withOptions({
-          module: 'home',
-          'skip-controller': true
-        })
-        .on('end', done);
+        .withGenerators([
+          join(__dirname, '../generators/module'),
+          join(__dirname, '../generators/route'),
+          join(__dirname, '../generators/controller'),
+          join(__dirname, '../generators/view')
+        ])
+        .on('end', () => {
+          helpers
+            .run(join(__dirname, '../generators/route'), {
+              tmpdir: false
+            })
+            .withArguments(['test'])
+            .withOptions({
+              module: 'home',
+              'skip-controller': true
+            })
+            .on('end', done);
+        });
     });
 
     it('should create route files', () => {
@@ -60,19 +58,39 @@ describe('Route generator', () => {
   describe('with Jade markup, CSS style, Coffee app, and Coffee test with module-type', () => {
     before((done) => {
       helpers
-        .run(join(__dirname, '../generators/route'), {
-          tmpdir: false
-        })
-        .withArguments(['test1'])
-        .withOptions({
-          structure: 'module-type',
-          module: 'home',
+        .run(join(__dirname, '../generators/app'))
+        .withPrompts({
+          appName: 'temp-route',
           markup: 'jade',
+          appScript: 'coffee',
+          controllerAs: false,
+          testScript: 'coffee',
+          testDir: 'app',
           style: 'css',
-          'app-script': 'coffee',
-          'test-script': 'coffee'
+          bower: []
         })
-        .on('end', done);
+        .withGenerators([
+          join(__dirname, '../generators/module'),
+          join(__dirname, '../generators/route'),
+          join(__dirname, '../generators/controller'),
+          join(__dirname, '../generators/view')
+        ])
+        .on('end', () => {
+          helpers
+            .run(join(__dirname, '../generators/route'), {
+              tmpdir: false
+            })
+            .withArguments(['test1'])
+            .withOptions({
+              structure: 'module-type',
+              module: 'home',
+              markup: 'jade',
+              style: 'css',
+              'app-script': 'coffee',
+              'test-script': 'coffee'
+            })
+            .on('end', done);
+        });
     });
 
     it('should create route files', () => {
@@ -87,29 +105,49 @@ describe('Route generator', () => {
     });
   });
 
-  describe('with HAML markup, SCSS style, JS app, and JS test', () => {
+  describe('with HAML markup, SCSS style, ES6 app, and ES6 test', () => {
     before((done) => {
       helpers
-        .run(join(__dirname, '../generators/route'), {
-          tmpdir: false
-        })
-        .withArguments(['test.child'])
-        .withOptions({
-          module: 'home',
+        .run(join(__dirname, '../generators/app'))
+        .withPrompts({
+          appName: 'temp-route',
           markup: 'haml',
-          style: 'scss'
+          appScript: 'es6',
+          controllerAs: false,
+          testScript: 'es6',
+          testDir: 'app',
+          style: 'scss',
+          bower: []
         })
-        .on('end', done);
+        .withGenerators([
+          join(__dirname, '../generators/module'),
+          join(__dirname, '../generators/route'),
+          join(__dirname, '../generators/controller'),
+          join(__dirname, '../generators/view')
+        ])
+        .on('end', () => {
+          helpers
+            .run(join(__dirname, '../generators/route'), {
+              tmpdir: false
+            })
+            .withArguments(['test.child'])
+            .withOptions({
+              module: 'home',
+              markup: 'haml',
+              style: 'scss'
+            })
+            .on('end', done);
+        });
     });
 
     it('should create route files', () => {
       assert.file([
-        'app/home/test-child-controller.js',
-        'app/home/test-child-controller_test.js',
+        'app/home/test-child-controller.es6',
+        'app/home/test-child-controller_test.es6',
         'app/home/test-child.tpl.haml',
         'app/home/test-child.scss',
-        'e2e/test-child/test-child.po.js',
-        'e2e/test-child/test-child_test.js'
+        'e2e/test-child/test-child.po.es6',
+        'e2e/test-child/test-child_test.es6'
       ]);
     });
   });
@@ -117,15 +155,35 @@ describe('Route generator', () => {
   describe('with HTML markup, Stylus style, JS app, and JS test', () => {
     before((done) => {
       helpers
-        .run(join(__dirname, '../generators/route'), {
-          tmpdir: false
+        .run(join(__dirname, '../generators/app'))
+        .withPrompts({
+          appName: 'temp-route',
+          markup: 'html',
+          appScript: 'js',
+          controllerAs: false,
+          testScript: 'js',
+          testDir: 'app',
+          style: 'styl',
+          bower: []
         })
-        .withArguments(['test'])
-        .withOptions({
-          module: 'app',
-          style: 'styl'
-        })
-        .on('end', done);
+        .withGenerators([
+          join(__dirname, '../generators/module'),
+          join(__dirname, '../generators/route'),
+          join(__dirname, '../generators/controller'),
+          join(__dirname, '../generators/view')
+        ])
+        .on('end', () => {
+          helpers
+            .run(join(__dirname, '../generators/route'), {
+              tmpdir: false
+            })
+            .withArguments(['test'])
+            .withOptions({
+              module: 'app',
+              style: 'styl'
+            })
+            .on('end', done);
+        });
     });
 
     it('should create route files', () => {
@@ -143,18 +201,38 @@ describe('Route generator', () => {
   describe('with Jade markup, CSS style, TypeScript app, and TypeScript test', () => {
     before((done) => {
       helpers
-        .run(join(__dirname, '../generators/route'), {
-          tmpdir: false
-        })
-        .withArguments(['test2'])
-        .withOptions({
-          module: 'home',
+        .run(join(__dirname, '../generators/app'))
+        .withPrompts({
+          appName: 'temp-route',
           markup: 'jade',
+          appScript: 'ts',
+          controllerAs: false,
+          testScript: 'ts',
+          testDir: 'app',
           style: 'css',
-          'app-script': 'ts',
-          'test-script': 'ts'
+          bower: []
         })
-        .on('end', done);
+        .withGenerators([
+          join(__dirname, '../generators/module'),
+          join(__dirname, '../generators/route'),
+          join(__dirname, '../generators/controller'),
+          join(__dirname, '../generators/view')
+        ])
+        .on('end', () => {
+          helpers
+            .run(join(__dirname, '../generators/route'), {
+              tmpdir: false
+            })
+            .withArguments(['test2'])
+            .withOptions({
+              module: 'home',
+              markup: 'jade',
+              style: 'css',
+              'app-script': 'ts',
+              'test-script': 'ts'
+            })
+            .on('end', done);
+        });
     });
 
     it('should create route files', () => {
