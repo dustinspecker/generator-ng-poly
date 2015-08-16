a8f37ec71ebc17847beb9492e20ad366bd399949
Dustin Specker <dustinspecker@dustinspecker.com>
2015-08-15 20:47:26 -0500

    feat(utils-prompts): add getBowerComponents function
    Enables easier testing of Bower component selection.

--- test/test-utils-prompts.js
+++ test/test-utils-prompts.js
@@ -4,6 +4,81 @@ import {expect} from 'chai';
 import promptsUtils from '../generators/utils/prompts';
 
 describe('Prompt Utils', () => {
+  let bowerComponentsForAllVersions;
+
+  bowerComponentsForAllVersions = [
+    {
+      name: 'Angular Animate',
+      value: 'animate'
+    },
+    {
+      name: 'Angular Cookies',
+      value: 'cookies'
+    },
+    {
+      name: 'Angular Resource',
+      value: 'resource'
+    },
+    {
+      name: 'Angular Sanitize',
+      value: 'sanitize'
+    },
+    {
+      name: 'Angular Touch',
+      value: 'touch'
+    },
+    {
+      name: 'Font Awesome',
+      value: 'fontawesome'
+    },
+    {
+      name: 'Lo-Dash',
+      value: 'lodash'
+    },
+    {
+      name: 'Restangular (installs Lo-Dash)',
+      value: 'restangular'
+    }
+  ];
+
+  describe('getBowerComponents', () => {
+    it('should return components avilable for any Angular version', () => {
+      expect(promptsUtils.getBowerComponents()).to.eql(bowerComponentsForAllVersions);
+    });
+
+    describe('Angular ^1.3.* copmonents', () => {
+      let angularAria, angularMessages;
+
+      angularAria = {
+        name: 'Angular Aria',
+        value: 'aria'
+      };
+
+      angularMessages = {
+        name: 'Angular Messages',
+        value: 'messages'
+      };
+
+      it('should not return Angular Aria and Messages for 1.2.*', () => {
+        expect(promptsUtils.getBowerComponents()).to.eql(bowerComponentsForAllVersions);
+      });
+
+      it('should return Angular Aria and Messages for 1.3.*', () => {
+        const bowerComponentsFor13 = promptsUtils.getBowerComponents('1.3.*');
+        expect(bowerComponentsFor13[1]).to.eql(angularAria);
+        expect(bowerComponentsFor13[3]).to.eql(angularMessages);
+        expect(bowerComponentsFor13.length).to.eql(10);
+      });
+
+      it('should return Angular Aria and Messages for 1.4.*', () => {
+        const bowerComponentsFor14 = promptsUtils.getBowerComponents('1.4.*');
+        expect(bowerComponentsFor14[1]).to.eql(angularAria);
+        expect(bowerComponentsFor14[3]).to.eql(angularMessages);
+        expect(bowerComponentsFor14.length).to.eql(10);
+      });
+    });
+  });
+
   describe('getUIFrameworks', () => {
     let frameworksForAllVersions;
 
