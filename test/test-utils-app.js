7909505c9dd65a8d0dc4d6db1ef181fc5d198398
Dustin Specker <dustinspecker@dustinspecker.com>
2015-10-17 20:07:34 -0500

    refactor(utils-app): use getYoPath instead of requiring it as param
    Add additional tests for utils-app.

--- test/test-utils-app.js
+++ test/test-utils-app.js
@@ -1,26 +1,58 @@
-/* global describe, it */
+/* global describe, beforeEach, afterEach, it */
 'use strict';
 import {expect} from 'chai';
 import {expectRequire} from 'a';
+import path from 'path';
 import proxyquire from 'proxyquire';
+import sinon from 'sinon';
 
 describe('App Utils', () => {
+  let moduleUtilsStub, utilsProxy;
+
+  beforeEach(() => {
+    moduleUtilsStub = {
+      getYoPath: sinon.stub().returns('awesome-project')
+    };
+
+    // mock out path to avoid needing to use file system to find build.config.json
+    utilsProxy = proxyquire('../generators/utils/app', {
+      './module': moduleUtilsStub
+    });
+
+    sinon.spy(path, 'join');
+  });
+
+  afterEach(() => {
+    path.join.restore();
+  });
+
+  describe('getAppName', () => {
+    it('should return app name', () => {
+      expectRequire('awesome-project/package.json').return({name: 'awesomeProject'});
+
+      expect(utilsProxy.getAppName()).to.eql('awesomeProject');
+      expect(moduleUtilsStub.getYoPath.calledOnce).to.eql(true);
+      expect(path.join.calledWith('awesome-project', 'package.json')).to.eql(true);
+    });
+  });
+
   describe('getAppDir', () => {
     it('should return app dir', () => {
-      let pathStub, utilsProxy;
+      expectRequire('awesome-project/build.config.js').return({appDir: 'app'});
 
-      pathStub = {
-        join() {
-          return 'build.config.js';
-        }
-      };
+      expect(utilsProxy.getAppDir()).to.eql('app');
+      expect(moduleUtilsStub.getYoPath.calledOnce).to.eql(true);
+      expect(path.join.calledWith('awesome-project', 'build.config.js')).to.eql(true);
+    });
+  });
 
-      // mock out path to avoid needing to use file system to find build.config.json
-      utilsProxy = proxyquire('../generators/utils/app', {path: pathStub});
+  describe('getUnitTestDir', () => {
+    it('should return unit test dir', () => {
+      expectRequire('awesome-project/build.config.js').return({unitTestDir: 'test'});
 
-      expectRequire('build.config.js').return({appDir: 'app'});
-
-      expect(utilsProxy.getAppDir('test')).to.eql('app');
+      expect(utilsProxy.getUnitTestDir()).to.eql('test');
+      expect(moduleUtilsStub.getYoPath.calledOnce).to.eql(true);
+      expect(path.join.calledWith('awesome-project', 'build.config.js')).to.eql(true);
     });
   });
 });
