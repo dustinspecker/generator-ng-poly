d8f29df5a23c1d7a5877b2de03504f7da5ce0c93
Jeffarese <jeff@wallapop.com>
2016-06-02 11:43:55 +0200

    Added Scope Interface, void typedefs and formatted docs properly

--- lib/directive/templates/_directive.ts
+++ lib/directive/templates/_directive.ts
@@ -1,7 +1,10 @@
 ///<reference path='<%= referencePath %>' />
 module <%= upperCamel %> {
     'use strict';
-
+    <% if (!controllerAs) { %>
+    interface I<%= upperCamel %>Scope extends ng.IScope {
+        <%= lowerCamel %>: any,
+    }<% } %>
     function <%= upperCamel %>Directive(): ng.IDirective {
         return {
             restrict: 'EA',
@@ -11,15 +14,14 @@ module <%= upperCamel %> {
             replace: false,<% if (controllerAs) { %>
             controllerAs: '<%= lowerCamel %>',<% } %>
             controller: <% if (!controllerAs) { %>
-                function($scope: ng.IScope) {
+                function($scope: I<%= upperCamel %>Scope): void {
                     $scope.<%= lowerCamel %> = {};
                     $scope.<%= lowerCamel %>.name = '<%= lowerCamel %>';
-                }
-                <% } else { %><%= upperCamel %>Controller<% } %>,
-            link: function (scope: ng.IScope, element: JQuery, attrs: any) {
+                },<% } else { %><%= upperCamel %>Controller,<% } %>
+            link: function (scope: ng.IScope, element: JQuery, attrs: any): void {
                 /*jshint unused:false */
                 /*eslint "no-unused-vars": [2, {"args": "none"}]*/
-            }
+            },
         }
     }
     <% if (controllerAs) { %>
@@ -41,11 +43,11 @@ module <%= upperCamel %> {
     * @description
     *
     * @example
-    <example module="<% if (parentModuleName) { %><%= parentModuleName %>.<% } %><%= moduleName %>">
-    <file name="index.html">
-    <<%= hyphenName %>></<%= hyphenName %>>
-    </file>
-    </example>
+    *   <example module="<% if (parentModuleName) { %><%= parentModuleName %>.<% } %><%= moduleName %>">
+    *       <file name="index.html">
+    *           <<%= hyphenName %>></<%= hyphenName %>>
+    *       </file>
+    *   </example>
     *
     */
     angular
